72f09c6bbfdb6cf0704f41f78b372280bacf8222
Altheabru04 <althea.abrusci@du.edu>
2025-11-11 09:36:02 -0700

    clean up

--- HW2/target/debug/HW2.d
+++
@@ -1,1 +0,0 @@
-C:\Users\althe\OneDrive\Desktop\Fall\ 2025\Rust\ Programming\HW2\target\debug\HW2.exe: C:\Users\althe\OneDrive\Desktop\Fall\ 2025\Rust\ Programming\HW2\src\main.rs
--- HW2/target/debug/deps/HW2-1759b9017c9e71af.d
+++
@@ -1,5 +0,0 @@
-C:\Users\althe\OneDrive\Desktop\Fall 2025\Rust Programming\HW2\target\debug\deps\HW2-1759b9017c9e71af.d: src\main.rs
-
-C:\Users\althe\OneDrive\Desktop\Fall 2025\Rust Programming\HW2\target\debug\deps\HW2-1759b9017c9e71af.exe: src\main.rs
-
-src\main.rs:
