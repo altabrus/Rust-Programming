b7922d893584075dc3f1c3d6c11e50b46d3eae00
Altheabru04 <althea.abrusci@du.edu>
2025-11-11 08:51:29 -0700

    HW1 rust

--- HW1/target/debug/deps/rust_functions.d
+++ HW1/target/debug/deps/rust_functions.d
@@ -1,5 +1,5 @@
-C:\Users\althe\OneDrive\Desktop\Fall 2025\Rust Programming\rust_functions\target\debug\deps\rust_functions.d: src\main.rs
+C:\Users\althe\OneDrive\Desktop\Fall 2025\Rust Programming\HW1\target\debug\deps\rust_functions.d: src\main.rs
 
-C:\Users\althe\OneDrive\Desktop\Fall 2025\Rust Programming\rust_functions\target\debug\deps\rust_functions.exe: src\main.rs
+C:\Users\althe\OneDrive\Desktop\Fall 2025\Rust Programming\HW1\target\debug\deps\rust_functions.exe: src\main.rs
 
 src\main.rs:
